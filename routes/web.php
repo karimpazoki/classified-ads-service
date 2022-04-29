14f0077de0d2241cbe51f987ef70b268d966c28a
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-30 00:54:42 +0430

    Add authentication routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group([
+    'prefix' => 'auth'
+], function ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->get('user', 'AuthController@user');
 });
