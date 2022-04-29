0c0704bd21df2faf18c0ff81063a6e67e0609e25
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-29 19:08:10 +0430

    initial database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Country;
+use App\Models\Province;
+use App\Models\City;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        Country::factory()
+            ->count(2)
+            ->has(Province::factory()
+                ->count(3)
+                ->has(
+                    City::factory()
+                    ->count(3)
+                    ->hasUsers(5)
+                )
+            )
+            ->create();
     }
 }
