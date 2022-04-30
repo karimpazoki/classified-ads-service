5c1d80489608407c835ee996b4d510216242e517
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-30 10:04:43 +0430

    Add category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Category;
+use App\Traits\ResponseBuilder;
+
+class CategoryController extends Controller
+{
+    use ResponseBuilder;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * returns a list of all categories' information
+     *
+     * @return App\Models\Category[]
+     */
+    public function index()
+    {
+        $categories = Category::where('parent_id', null)
+            ->with('children')
+            ->get();
+        return $this->success(["categories" => $categories]);
+    }
+
+    /**
+     * Returns an category's information
+     *
+     * @param int category
+     *
+     * @return App\Models\Category
+     */
+    public function show($category)
+    {
+        return $this->success(["category" => Category::with('children')->findOrFail($category)]);
+    }
+
+    /**
+     * Stores new category's information
+     *
+     * @param Illuminate\Http\Request $request
+     *
+     * @return App\Models\Category
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|string|max:255',
+        ]);
+
+        return $this->success(["category" => Category::create($request->all())], "The category created successfully", Response::HTTP_CREATED);
+    }
+
+    /**
+     * Updates an existing category's information
+     *
+     * @param Illuminate\Http\Request $request
+     * @param int category
+     *
+     * @return App\Models\Category
+     */
+    public function update(Request $request, $category)
+    {
+        $this->validate($request,[
+            'name' => 'string|max:255',
+        ]);
+
+        $category = Category::findOrFail($category);
+        $category->fill($request->all());
+        $category->save();
+        return $this->success(["category" => $category], "The category updated successfully", Response::HTTP_OK);
+    }
+
+    /**
+     * Removes an existing category
+     *
+     * @param int category
+     *
+     * @return App\Models\Category
+     */
+    public function delete($category)
+    {
+        $category = Category::findOrFail($category);
+        $category->delete();
+        return $this->success(["category" => $category], "The \"{$category}\" category deleted successfully", Response::HTTP_OK);
+    }
+}
