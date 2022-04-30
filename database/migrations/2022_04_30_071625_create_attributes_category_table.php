e64fca3116794985ca20a44c70053055f5a1f1d8
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-30 12:42:12 +0430

    Change attributes categories table name

--- database/migrations/2022_04_30_071625_create_attributes_category_table.php
+++ database/migrations/2022_04_30_071625_create_attributes_category_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('attributes_category', function (Blueprint $table) {
+        Schema::create('attributes_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
             $table->foreignId('parent_id', 255)->nullable();
