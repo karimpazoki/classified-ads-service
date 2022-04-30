972342b83eec6f4a29c1d66b743ddc2d58a84050
Karim Pazoki <pazoki.karim72@gmail.com>
2022-04-30 09:29:39 +0430

    Add category model

---
+++ app/Models/Category.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\{
+    BelongsTo,
+    HasMany
+};
+
+class Category extends Model 
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'parent_id'
+    ];
+
+    /**
+     * Get the parent that owns the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'id', 'parent_id');
+    }
+
+    
+    /**
+     * Get all of the children for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function children(): HasMany
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
+    }
+}
