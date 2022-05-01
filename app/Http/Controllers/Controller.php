230bfb028148a9166bd3df43c228f9a952f6b6a0
Karim Pazoki <pazoki.karim72@gmail.com>
2022-05-01 13:26:21 +0430

    Initial swagger annotaion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,15 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @OA\Info(
+     *   title="Classified ads API",
+     *   version="1.0",
+     *   description="Classified ads platform help people list products and services for sale.",
+     *   @OA\Contact(
+     *     email="pazoki.karim72@gmail.com",
+     *     name="Karim Pazoki"
+     *   )
+     * )
+     */
 }
