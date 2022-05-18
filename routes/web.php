a3ba39d4f2f7f7669cd077f5e4a9b8cc00adffb1
Karim Pazoki <pazoki.karim72@gmail.com>
2022-05-18 17:36:15 +0430

    A little clean up in route  file format

--- routes/web.php
+++ routes/web.php
@@ -30,48 +30,72 @@ $router->group(
     ],
     function () use ($router) {
         // Brands
-        $router->get('/brand', 'BrandController@index');
-        $router->post('/brand', 'BrandController@store');
-        $router->get('/brand/{brand}', 'BrandController@show');
-        $router->put('/brand/{brand}', 'BrandController@update');
-        $router->patch('/brand/{brand}', 'BrandController@update');
-        $router->delete('/brand/{brand}', 'BrandController@delete');
+        $router->group([
+            'prefix' => 'brand'
+        ], function ($router) {
+            $router->get('/', 'BrandController@index');
+            $router->post('/', 'BrandController@store');
+            $router->get('/{brand}', 'BrandController@show');
+            $router->put('/{brand}', 'BrandController@update');
+            $router->patch('/{brand}', 'BrandController@update');
+            $router->delete('/{brand}', 'BrandController@delete');
+        });
 
         //Categories
-        $router->get('/category', 'CategoryController@index');
-        $router->post('/category', 'CategoryController@store');
-        $router->get('/category/{category}', 'CategoryController@show');
-        $router->put('/category/{category}', 'CategoryController@update');
-        $router->patch('/category/{category}', 'CategoryController@update');
-        $router->delete('/category/{category}', 'CategoryController@delete');
-        $router->post('/category/{category}/attributes-category', 'CategoryController@attachAttributeCategories');
+        $router->group([
+            'prefix' => 'category'
+        ], function ($router) {
+            $router->get('/', 'CategoryController@index');
+            $router->post('/', 'CategoryController@store');
+            $router->get('/{category}', 'CategoryController@show');
+            $router->put('/{category}', 'CategoryController@update');
+            $router->patch('/{category}', 'CategoryController@update');
+            $router->delete('/{category}', 'CategoryController@delete');
+            $router->post('/{category}/attributes-category', 'CategoryController@attachAttributeCategories');
+        });
 
         //Attributes Categories
-        $router->get('/attributes-category', 'AttributesCategoryController@index');
-        $router->post('/attributes-category', 'AttributesCategoryController@store');
-        $router->get('/attributes-category/{category}', 'AttributesCategoryController@show');
-        $router->put('/attributes-category/{category}', 'AttributesCategoryController@update');
-        $router->patch('/attributes-category/{category}', 'AttributesCategoryController@update');
-        $router->delete('/attributes-category/{category}', 'AttributesCategoryController@delete');
+        $router->group([
+            'prefix' => 'attributes-category'
+        ], function ($router) {
+            $router->get('/', 'AttributesCategoryController@index');
+            $router->post('/', 'AttributesCategoryController@store');
+            $router->get('/{category}', 'AttributesCategoryController@show');
+            $router->put('/{category}', 'AttributesCategoryController@update');
+            $router->patch('/{category}', 'AttributesCategoryController@update');
+            $router->delete('/{category}', 'AttributesCategoryController@delete');
+        });
 
         // Location
-        $router->get('/city', 'CityController@index');
-        $router->get('/city/{city}', 'CityController@show');
-        $router->get('/province', 'ProvinceController@index');
-        $router->get('/province/{province}', 'ProvinceController@show');
-        $router->get('/country', 'CountryController@index');
-        $router->get('/country/{country}', 'CountryController@show');
+        $router->group([
+            'prefix' => 'location'
+        ], function ($router) {
+            $router->get('/city', 'CityController@index');
+            $router->get('/city/{city}', 'CityController@show');
+            $router->get('/province', 'ProvinceController@index');
+            $router->get('/province/{province}', 'ProvinceController@show');
+            $router->get('/country', 'CountryController@index');
+            $router->get('/country/{country}', 'CountryController@show');
+        });
 
         // Field-types
-        $router->get('/field-type', 'FieldTypeController@index');
-        $router->get('/field-type/{field_type}', 'FieldTypeController@show');
+        $router->group([
+            'prefix' => 'field-type'
+        ], function ($router) {
+            $router->get('/', 'FieldTypeController@index');
+            $router->get('/{field_type}', 'FieldTypeController@show');
+        });
 
         // Attributes
-        $router->get('/attribute', 'AttributeController@index');
-        $router->post('/attribute', 'AttributeController@store');
-        $router->get('/attribute/{attribute}', 'AttributeController@show');
-        $router->put('/attribute/{attribute}', 'AttributeController@update');
-        $router->patch('/attribute/{attribute}', 'AttributeController@update');
-        $router->delete('/attribute/{attribute}', 'AttributeController@delete');
+        $router->group([
+            'prefix' => 'attribute'
+        ], function ($router) {
+            $router->get('/', 'AttributeController@index');
+            $router->post('/', 'AttributeController@store');
+            $router->get('/{attribute}', 'AttributeController@show');
+            $router->put('/{attribute}', 'AttributeController@update');
+            $router->patch('/{attribute}', 'AttributeController@update');
+            $router->delete('/{attribute}', 'AttributeController@delete');
+        });
     }
 );
